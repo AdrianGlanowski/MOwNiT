e372b63dc3145d5c1a77a381831546e453ca2e34
AdrianGlanowski <adrianglanowski04@gmail.com>
2025-03-26 18:26:32 +0100

    wiekszosc lab2

--- lab2/lab2.py
+++ lab2/lab2.py
@@ -1,26 +1,82 @@
 import numpy as np
+from time import time
 def solve(A: list[list], B: list):
     A = np.array(A).astype(float)
-    np.c_[A, B]
+    Gauss = np.c_[A, B]
+    result = [0]*len(B)
     
     already_satisfied = [] #tablica wierszy, ktore juz zrobilem, nie ruszac wiecej
 
-    for change in range(len(A)):
+    for change in range(len(Gauss)):
         
         #znajdz rzad z najw elementem
         maxi, max_index = 0, 0
-        for r in range(len(A)):
-            if abs(A[r][change]) > maxi and r not in already_satisfied:
-                maxi = A[r][change]
+        for r in range(len(Gauss)):
+            if abs(Gauss[r][change]) > maxi and r not in already_satisfied:
+                maxi = Gauss[r][change]
                 max_index = r
         already_satisfied.append(max_index)
 
-        for r in range(len(A)):
+        for r in range(len(Gauss)):
             if r == max_index: continue
             #zmodyfikuj wspolczynniki
-            A[r] -=  (A[r][0]/maxi)*A[max_index] #= ta wartosc, na ktora mam zmienic
-        A[max_index] /= maxi
+            Gauss[r] -=  (Gauss[r][change]/maxi)*Gauss[max_index] #= ta wartosc, na ktora mam zmienic
+        Gauss[max_index] /= maxi
     
-    return A
+    for i in range(len(B)):
+        result[i] = Gauss[already_satisfied[i]][-1]
+
+    return result
                 
-print(solve([[2, 2, -1, 1],[-1, 1, 2, 3],[3, -1, 4, -1],[1, 4, -2, 2]], [7, 3, 31, 2]))
+def ex_1_test():
+    for i in range(10):
+        print("TEST ", i, "WIELKOSC: ", 500 + i*50)
+
+        A = np.random.rand(500 + i*50, 500 + i*50)
+        B = np.random.rand(500 + i*50, 1)
+
+        start = time()
+        solve(A, B)
+        end = time()
+        print("\tCzas mojej implementacji: ", end - start)
+    
+        start = time()
+        np.linalg.solve(A, B)
+        end = time()
+        print("\tCzas funkcji biblioteczej: ", end - start)
+
+#ex_1_test()
+
+def LU_factorization(A: list[list]):
+    Gauss = np.array(A).astype(float)
+    
+    for change in range(len(Gauss)):
+        for r in range(change+1, len(Gauss)):
+            #zmodyfikuj wspolczynniki
+            factor = (Gauss[r][change]/Gauss[change][change])
+            Gauss[r][change:] -=  factor*Gauss[change][change:] #= ta wartosc, na ktora mam zmienic
+            Gauss[r][change] = factor
+
+    L = np.tril(Gauss)
+    for i in range(len(L)):
+        L[i][i] = 1
+    U = np.triu(Gauss)
+
+    return L, U
+
+def ex_2_test():
+    tests = ""
+    for i in range(10):
+        print("TEST ", i, "WIELKOSC: ", 500 + i*50)
+
+        A = np.random.rand(500 + i*50, 500 + i*50)
+        L, U = LU_factorization(A)
+
+        if np.linalg.det(A - np.matmul(L, U)) != 0:
+            print("Wyznacznik różny od 0: ", np.det(A - np.matmul(L, U)))
+            tests = tests + "W"
+        else:
+            tests = tests + "A"
+    return len(tests), tests
+
+print(ex_2_test())
